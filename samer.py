e37af38f0d152a8c49e2fcc54ef98cd752b9d5bc
Samer <i.samer14231@gmail.com>
2025-02-19 01:23:29 +0300

    Update and rename ss to samer.py

---
+++ samer.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import plotly.graph_objects as go
+import dash
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output
+
+
+airline_data =  pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/airline_data.csv', 
+                            encoding = "ISO-8859-1",
+                            dtype={'Div1Airport': str, 'Div1TailNum': str, 
+                                   'Div2Airport': str, 'Div2TailNum': str})
+
+
+
+# Create a dash application
+app = dash.Dash(__name__)
+app.layout = html.Div(children=[html.H1('Airline Performance Dashboard',style={'textAlign': 'center', 'color': '#503D36', 'font-size': 40}),
+                                html.Div(["Input Year", dcc.Input(),], 
+                                style={}),
+                                html.Br(),
+                                html.Br(),
+                                html.Div(),
+                                ])
+# Run the app
+if __name__ == '__main__':
+   app.run_server( port=8053)
